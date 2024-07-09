d7f081291591e72ca15e8a3dfc8e292cccb67155
Егор Северов <egorseveroff@gmail.com>
2024-07-09 17:32:16 +0400

    level 7 bit finish

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,16 +1,20 @@
-import genDiff from '../__fixtures__/gendiff-code.js';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+import genDiff from '../src/gendiff-code.js';
 
-test('gendiff', () => {
-  const obj1 = {
-    host: 'hexlet.io',
-    timeout: 50,
-    proxy: '123.234.53.22',
-    follow: false,
-  };
-  const obj2 = {
-    timeout: 20,
-    verbose: true,
-    host: 'hexlet.io',
-  };
-  expect(genDiff(obj1, obj2)).toEqual('{\n - follow: false\n   host: hexlet.io\n - proxy: 123.234.53.22\n - timeout: 50\n + timeout: 20\n + verbose: true\n}');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+
+const firstFile = getFixturePath('file1.json');
+const secondFile = getFixturePath('file2.json');
+const getResult = readFile('resultForStylish.txt');
+const format = 'stylish';
+const result = genDiff(firstFile, secondFile, format);
+expect(result).toEqual(getResult);
+test('genDiff', () => {
+  expect(genDiff(result)).toEqual(getResult);
 });
