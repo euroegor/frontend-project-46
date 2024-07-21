b332ef8d3a79373b2289f3f9c5ccb11b419a8863
Егор Северов <egorseveroff@gmail.com>
2024-07-21 14:30:19 +0400

    make test with each

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -9,35 +9,17 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-const firstFile = getFixturePath('file1.json');
-const secondFile = getFixturePath('file2.json');
-const getResultA = readFile('resultForStylish.txt').trim();
-const getResultB = readFile('resultForPlain.txt');
-const formatA = 'stylish';
-const formatB = 'plain';
-const resultA = genDiff(firstFile, secondFile, formatA);
-const resultB = genDiff(firstFile, secondFile, formatB);
-test('genDiffAB', () => {
-  expect(resultA).toEqual(getResultA);
-  expect(resultB).toEqual(getResultB);
-});
-const firstFile2 = getFixturePath('file1.yaml');
-const secondFile2 = getFixturePath('file2.yaml');
-const getResultC = readFile('resultForStylish.txt').trim();
-const getResultD = readFile('resultForPlain.txt');
-const formatC = 'stylish';
-const formatD = 'plain';
-const resultC = genDiff(firstFile2, secondFile2, formatC);
-const resultD = genDiff(firstFile2, secondFile2, formatD);
-test('genDiffCD', () => {
-  expect(resultC).toEqual(getResultC);
-  expect(resultD).toEqual(getResultD);
-});
-const firstFile3 = getFixturePath('file1.yaml');
-const secondFile3 = getFixturePath('file2.json');
-const getResult = readFile('resultForJson.txt');
-const format = 'json';
-const result = genDiff(firstFile3, secondFile3, format);
-test('genDiff', () => {
+const testcases = [
+  ['file1.json', 'file2.json', 'resultForJson.txt', 'json'],
+  ['file1.yaml', 'file2.yaml', 'resultForStylish.txt', 'stylish'],
+  ['file1.json', 'file2.json', 'resultForStylish.txt', 'stylish'],
+  ['file1.json', 'file2.json', 'resultForPlain.txt', 'plain'],
+];
+
+test.each(testcases)('Compare %s and %s to expect %s in "%s" style', (file1, file2, output, style) => {
+  const firstFile = getFixturePath(file1);
+  const secondFile = getFixturePath(file2);
+  const getResult = readFile(output).trim();
+  const result = genDiff(firstFile, secondFile, style);
   expect(result).toEqual(getResult);
 });
